eebb85997ba500590ddfc5d62f002ef042332e3b
Leica Florian Robert <florian.leica@gmail.com>
2020-10-13 23:07:06 +0200

    Added config for new congregations

---
+++ extra/schiomagre_data.php
@@ -0,0 +1,5 @@
+<?php
+	define("NAME", "Schio Magrè"); 
+	define("ID", "schiomagre");
+	define("MAIN_FOLDER_ID", "1rASl_ee38ZaJ58ZwLAstgWMVq-MFhLfE"); //Codice cartella principale della Bacheca di Google Drive;
+?>
---
+++ extra/schiopasubio_data.php
@@ -0,0 +1,5 @@
+<?php
+	define("NAME", "Schio Pasubio"); 
+	define("ID", "schiopasubio");
+	define("MAIN_FOLDER_ID", "0BxpVF7g1Csc4MGI3REkwU0lQVTQ"); //Codice cartella principale della Bacheca di Google Drive;
+?>
